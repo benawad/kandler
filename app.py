c3e6c2869b3007f916319f487d89cc68ddf47be9
ben awad <benawad97@gmail.com>
2016-07-23 14:53:38 -0500

    Add webhook

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Hi guys!'
+    return 'Hello!'
+
+@app.route("/webhook")
+def verify():
+    print(request.args)
+    return str(request.args)
 
 if __name__ == "__main__":
     app.run()
