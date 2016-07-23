debe66905f54ccc1d69ccfd412990bbacad787c9
ben awad <benawad97@gmail.com>
2016-07-23 17:07:45 -0500

    Load data then get form

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ def hello():
 @app.route("/webhook", methods=['POST', 'GET'])
 def verify():
     if request.method == 'POST':
-        data = json.loads(str(request.data))
-        message1 = data['entry'][0]['messaging'][0]
+        request.get_data()
+        print(request.form)
+        # data = json.loads(str(request.data))
+        # message1 = data['entry'][0]['messaging'][0]
         # message['recipient'] = {'id':request.data['entry']}
-        print(request.data)
-        return message1, 200
+        # print(request.data)
+        return request.form['entry'][0]['messaging'][0], 200
     else:
         token = request.args.get('hub.verify_token', '')
         mode = request.args.get('hub.mode', '')
