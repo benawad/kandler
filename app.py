a6fba5d72dc8a88f0d94e58bb6739e290f1e0bf1
ben awad <benawad97@gmail.com>
2016-07-23 15:15:58 -0500

    post

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ app = Flask(__name__)
 def hello():
     return 'Hello!'
 
-@app.route("/webhook")
+@app.route("/webhook", methods=['POST', 'GET'])
 def verify():
-    token = request.args.get('hub.verify_token', '')
-    mode = request.args.get('hub.mode', '')
-    challenge = request.args.get('hub.challenge', '')
-    correct_token = 'japanese_sausage_plant'
-
-    if token == correct_token and mode == 'subscribe':
-        return challenge
+    if request.method == 'POST':
+        print(request.form)
+        return str(request.form), 200
     else:
-        return "Something went wrong :("
+        token = request.args.get('hub.verify_token', '')
+        mode = request.args.get('hub.mode', '')
+        challenge = request.args.get('hub.challenge', '')
+        correct_token = 'japanese_sausage_plant'
+
+        if token == correct_token and mode == 'subscribe':
+            return challenge, 200
+        else:
+            return "Something went wrong :(", 403
 
 if __name__ == "__main__":
     app.run()
