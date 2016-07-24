06376763cd64dd0cb9f274471a5febca9001e934
ben awad <benawad97@gmail.com>
2016-07-23 19:43:05 -0500

    loop

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ def hello():
 @app.route("/webhook", methods=['POST', 'GET'])
 def verify():
     if request.method == 'POST':
-        data = request.get_json()
-        message = data['entry'][0]['messaging'][0]
-        params = {'access_token': 'EAAPQfxYLKaMBANW7bFFb5amwHRiuhRGqoK8jPH0GPpHDyTWHNiguz8I3VQq6rjSfEbETuonZBENcndPiYwZCBF66QwBHRcvZBjgdsVVizGhMyOSY42J7KAlp5wX7e0a8KWbKUbZANnjpmVkRUQweVgtN6IsHcsQETdAmW2RfQwZDZD'}
         headers = {'Content-Type': 'application/json'}
-        a = {}
-        a['recipient'] = {'id':message['sender']['id']}
-        a['message'] = {'text':message['message']['text']}
-        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=json.dumps(a))
-        print('sc: %s' % r.status_code)
-        print(r.text)
+        params = {'access_token': 'EAAPQfxYLKaMBANW7bFFb5amwHRiuhRGqoK8jPH0GPpHDyTWHNiguz8I3VQq6rjSfEbETuonZBENcndPiYwZCBF66QwBHRcvZBjgdsVVizGhMyOSY42J7KAlp5wX7e0a8KWbKUbZANnjpmVkRUQweVgtN6IsHcsQETdAmW2RfQwZDZD'}
+        data = request.get_json()
+        for m in data['entry'][0]['messaging']:
+            if 'message' in m:
+                a = {}
+                a['recipient'] = {'id':m['sender']['id']}
+                a['message'] = {'text':m['message']['text']}
+                r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=json.dumps(a))
+                print('sc: %s' % r.status_code)
+                print(r.text)
         return "ok!", 200
     else:
         token = request.args.get('hub.verify_token', '')
