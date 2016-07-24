21486f0ab54a8f908af7993c859ab2a27804df27
ben awad <benawad97@gmail.com>
2016-07-23 19:03:57 -0500

    guess

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ def hello():
 @app.route("/webhook", methods=['POST', 'GET'])
 def verify():
     if request.method == 'POST':
-        request.get_data()
-        print(request.form)
-        # data = json.loads(str(request.data))
+        print('form: %s' % request.form)
+        print('data: %s' % request.data)
+        data = json.loads(str(request.data))
         # message1 = data['entry'][0]['messaging'][0]
         # message['recipient'] = {'id':request.data['entry']}
-        # print(request.data)
-        return request.form['entry'][0]['messaging'][0], 200
+        return data['entry'][0]['messaging'][0], 200
     else:
         token = request.args.get('hub.verify_token', '')
         mode = request.args.get('hub.mode', '')
