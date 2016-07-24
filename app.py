14df1e9b5f2e8aa65cbb6c40a36e2aa425a0bac3
ben awad <benawad97@gmail.com>
2016-07-24 07:34:19 -0500

    Abstract stuff

--- app.py
+++ app.py
@@ -12,17 +12,10 @@ def hello():
 @app.route("/webhook", methods=['POST', 'GET'])
 def verify():
     if request.method == 'POST':
-        headers = {'Content-Type': 'application/json'}
-        params = {'access_token': os.environ['PAGE_ACCESS_TOKEN']}
         data = request.get_json()
         for m in data['entry'][0]['messaging']:
             if 'message' in m:
-                a = {}
-                a['recipient'] = {'id':m['sender']['id']}
-                a['message'] = {'text':m['message']['text']}
-                r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=json.dumps(a))
-                print('sc: %s' % r.status_code)
-                print(r.text)
+                send_message(m['sender']['id'], m['message']['text'])
         return "ok!", 200
     else:
         token = request.args.get('hub.verify_token', '')
@@ -34,6 +27,21 @@ def verify():
             return challenge, 200
         else:
             return "Something went wrong :(", 403
+
+def send_message(recipient_id, message):
+    message_data = {
+            'recipient': {'id' : recipient_id},
+            'message': {'text' : message}
+            }
+    message_json = json.dumps(message_data)
+    headers = {'Content-Type': 'application/json'}
+    params = {'access_token': os.environ['PAGE_ACCESS_TOKEN']}
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=message_data)
+    if r.status_code == 200:
+        print('Sent "%s" to %s' % (recipient_id, message))
+    else:
+        print('FAILED to send "%s" to %s' % (recipient_id, message))
+        print('REASON: %s' % r.text)
 
 if __name__ == "__main__":
     app.run()
