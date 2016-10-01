c5926386334f11a10be08409f7fe0173046719f2
ben awad <benawad97@gmail.com>
2016-10-01 17:54:05 -0500

    one_script

--- app.py
+++ app.py
@@ -10,9 +10,19 @@ from datetime import datetime
 import ystockquote
 import plotly.graph_objs as go
 import pandas.io.data as web
-from tasks import add
+from celery import Celery
 
 app = Flask(__name__)
+
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+@app.task
+def add(x, y):
+    return x + y
 
 @app.route("/")
 def hello():
