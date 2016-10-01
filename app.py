7189f9c22190307c185f744b867cd303ed295c76
ben awad <benawad97@gmail.com>
2016-10-01 13:21:27 -0500

    Send img

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ def hello():
 def verify():
     if request.method == 'POST':
         data = request.get_json()
+        # loop through unread messages
         for m in data['entry'][0]['messaging']:
             if 'message' in m:
-                send_message(m['sender']['id'], m['message']['text'])
+                # send_message(m['sender']['id'], m['message']['text'])
+                img_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4gAlQuMrYW2quJAXYEWydvHBzLMcFDY6HrcX_emZdJQfcA4UfEt4G374"
+                send_picture(m['sender']['id'], img_url)
         return "ok!", 200
     else:
         token = request.args.get('hub.verify_token', '')
@@ -30,9 +33,9 @@ def verify():
 
 def send_message(recipient_id, message):
     message_data = {
-            'recipient': {'id' : recipient_id},
-            'message': {'text' : message}
-            }
+        'recipient': {'id' : recipient_id},
+        'message': {'text' : message}
+    }
     headers = {'Content-Type': 'application/json'}
     params = {'access_token': os.environ['PAGE_ACCESS_TOKEN']}
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=json.dumps(message_data))
@@ -40,6 +43,27 @@ def send_message(recipient_id, message):
         print('Sent "%s" to %s' % (recipient_id, message))
     else:
         print('FAILED to send "%s" to %s' % (recipient_id, message))
+        print('REASON: %s' % r.text)
+
+def send_picture(recipient_id, img_url):
+    message_data = {
+        'recipient': {'id': recipient_id},
+        'message': {
+            'attachment': {
+                "type": "image",
+                "payload": {
+                        "url": img_url,
+                    }
+                }
+            }
+        }
+    headers = {'Content-Type': 'application/json'}
+    params = {'access_token': os.environ['PAGE_ACCESS_TOKEN']}
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=json.dumps(message_data))
+    if r.status_code == 200:
+        print('Sent "%s" to %s' % (recipient_id, img_url))
+    else:
+        print('FAILED to send "%s" to %s' % (recipient_id, img_url))
         print('REASON: %s' % r.text)
 
 if __name__ == "__main__":
